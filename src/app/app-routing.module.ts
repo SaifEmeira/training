28e3adadad56113b9fe2ff90c74a18ea155c428e
SaifEmeira <saifemeira@gmail.com>
2024-09-16 18:26:47 +0300

    fixing routing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './dashboard/components/notfound/notfound.component';
 
 const routes: Routes = [
   {path:'',redirectTo:"dashboard",pathMatch:"full"},
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, 
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {path:"**",component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash:true})],
